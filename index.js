1667f1594f56e75652f5c81f13c6a74d79674b7e
Troy Griffiths <troygriffi@gmail.com>
2017-07-06 10:15:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.static(__dirname + '/public'));
 //   response.render('pages/index');
 // });
 
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
